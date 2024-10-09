7611c89152604410cbf096bde9d8fbae5f5b3415
Diante Dowdell <113080355+ddowdell99@users.noreply.github.com>
2024-10-08 20:16:50 -0500

    adding flowbite and beginning to add navbar layout (search bar/icon, logo - WIP)

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import React from "react";
+
+function Header() {
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 border border-transparent bg-gradient-to-r from-black to-gray-600 text-white">
+          The Bent
+        </span>
+        Bindings
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="black">
+        <AiOutlineSearch />
+      </Button>
+    </Navbar>
+  );
+}
+
+export default Header;
