91e28b92c768d5886fe942fe092c874bf118e9ff
Diante Dowdell <113080355+ddowdell99@users.noreply.github.com>
2024-10-09 18:44:58 -0500

    finished header barebones with hamburger menu for mobile

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,12 @@
 import { Button, Navbar, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 import React from "react";
 
 function Header() {
+  {/* useLocation is used when you want to store (specifically below) the path to be used later. Such as /home or /about */}
+  const path = useLocation().pathname;
   return (
     <Navbar className="border-b-2">
       <Link
@@ -26,6 +29,27 @@ function Header() {
       <Button className="w-12 h-10 lg:hidden" color="black">
         <AiOutlineSearch />
       </Button>
+
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="black">
+          <FaMoon />
+        </Button>
+        <Link to="/sign-in">
+          <Button color="dark">Sign In</Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/reviews"} as={"div"}>
+          <Link to="/reviews">Reviews</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
